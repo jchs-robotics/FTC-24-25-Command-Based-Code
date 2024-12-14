2bafd2007377b71e87ed200acd2e98840456ff62
Lucas <cosmodapro@gmail.com>
2024-12-13 17:00:57 -1000

    Made netzone autos PATHS in MEEPMEEP. Translate them into ROADRUNNER and add manipulator commands.

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -19,7 +19,6 @@ enum Route {
 
 
 public class MeepMeepTesting {
-
     public static final Route route = Route.NET_ZONE_AUTO;
     public static final double maxVel = 60;
     public static final double maxAccel = 60;
@@ -28,25 +27,67 @@ public class MeepMeepTesting {
     public static final double trackWidth = 15;
 
 
-    public static final boolean red = false;
-    public static double netZoneX = -53;
-    public static double netZoneY = -53;
-    public static double sampleOneX = -37;
-    public static double sampleTwoX = -47;
-    public static double sampleThreeX = -57;
-    public static double sampleThreeY = -25;
+    public static final boolean red = true;
+
+    public static double startX;
+    public static double startY;
+    public static double startHeading;
+
+    public static double netZoneXY;
+    public static double netZoneHeading;
+
+    public static double sampleOneX;
+    public static double sampleTwoX;
+    public static double sampleThreeX;
+    public static double sampleY;
+    public static double fifteen;
+    public static double sampleHeading;
+
+    // Park parameters
+    public static double parkX;
+    public static double parkY = 0;
+    public static double parkHeading;
+
+    public static double parkTangent;
+
 
 
 
     public static void main(String[] args) {
 
-        if (!red) {
-            netZoneX *= -1;
-            netZoneY *= -1;
-            sampleOneX *= -1;
-            sampleTwoX *= -1;
-            sampleThreeX *= -1;
-            sampleThreeY *= -1;
+        if (red) {
+            startX = -30;
+            startY = -60;
+            startHeading = 180;
+
+            netZoneXY = -53;
+            netZoneHeading = Math.toRadians(45);
+            sampleOneX = -37;
+            sampleTwoX = -47;
+            sampleThreeX = -57;
+            sampleY = -25;
+            fifteen = 15;
+            sampleHeading = Math.toRadians(-180);
+            parkX = -25;
+            parkHeading = -160;
+            parkTangent = 0;
+
+        } else { // else if blue
+            startX = 30;
+            startY = 60;
+            startHeading = 0;
+
+            netZoneXY = 53;
+            netZoneHeading = Math.toRadians(-135);
+            sampleOneX = 37;
+            sampleTwoX = 47;
+            sampleThreeX = 57;
+            sampleY = 25;
+            fifteen = -15;
+            sampleHeading = 0;
+            parkX = 25;
+            parkHeading = 160;
+            parkTangent = 160;
         }
 
 
@@ -65,43 +106,44 @@ public class MeepMeepTesting {
 
 
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(30, 60, 0))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(startX, startY, startHeading))
 
                 // score preload
-                .strafeToLinearHeading(new Vector2d(53,53), Math.toRadians(-135))
+                .strafeToLinearHeading(new Vector2d(netZoneXY, netZoneXY), netZoneHeading)
                 .waitSeconds(2)
 
                 // line up with sample and intake
 //                .splineToLinearHeading(new Pose2d(37,25, Math.toRadians(0)), 0)
-                .strafeToLinearHeading(new Vector2d(37,40), Math.toRadians(0))
-                .strafeTo(new Vector2d(37, 25))
+                .strafeToLinearHeading(new Vector2d(sampleOneX,sampleY - fifteen), sampleHeading)
+                .strafeTo(new Vector2d(sampleOneX, sampleY))
                 .waitSeconds(2)
 
 
                 // line up with basket and score
-                .strafeToLinearHeading(new Vector2d(53,53), Math.toRadians(-135))
+                .strafeToLinearHeading(new Vector2d(netZoneXY,netZoneXY), netZoneHeading)
                 .waitSeconds(2)
+
 
                 // line up with sample 2 and intake
 //                .splineToLinearHeading(new Pose2d(47, 25, Math.toRadians(0)), 0)
-                .strafeToLinearHeading(new Vector2d(47,25), Math.toRadians(0))
+                .strafeToLinearHeading(new Vector2d(sampleTwoX,sampleY), sampleHeading)
                 .waitSeconds(2)
 
                 // line up with basket and score
-                .strafeToLinearHeading(new Vector2d(53,53), Math.toRadians(-135))
+                .strafeToLinearHeading(new Vector2d(netZoneXY,netZoneXY), netZoneHeading)
                 .waitSeconds(2)
 
                 // line up with sample 3 and intake
 //                .splineToLinearHeading(new Pose2d(57, 25, Math.toRadians(0)), 0)
-                .strafeToLinearHeading(new Vector2d(57,25), Math.toRadians(0))
+                .strafeToLinearHeading(new Vector2d(sampleThreeX,sampleY), sampleHeading)
                 .waitSeconds(2)
 
                 // line up with basket and score
-                .strafeToLinearHeading(new Vector2d(53,53), Math.toRadians(-135))
+                .strafeToLinearHeading(new Vector2d(netZoneXY,netZoneXY), netZoneHeading)
                 .waitSeconds(2)
 
                 // park
-                .splineToLinearHeading(new Pose2d(25, 0, Math.toRadians(180)), 160)
+                .splineToLinearHeading(new Pose2d(parkX, parkY, sampleHeading), parkTangent)
                 .waitSeconds(2)
 
 
